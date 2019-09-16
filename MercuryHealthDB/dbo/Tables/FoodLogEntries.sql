46c22a705cd7c0de5543927b90b342150a29b8ed
abel <abel.wang@gmail.com>
2019-09-15 19:55:40 -0700

    added color

--- MercuryHealthDB/dbo/Tables/FoodLogEntries.sql
+++ MercuryHealthDB/dbo/Tables/FoodLogEntries.sql
@@ -10,6 +10,7 @@
     [FatInGrams]           DECIMAL (18, 2) NOT NULL,
     [CarbohydratesInGrams] DECIMAL (18, 2) NOT NULL,
     [SodiumInGrams]        DECIMAL (18, 2) NOT NULL,
+    [Color] NVARCHAR(50) NULL, 
     CONSTRAINT [PK_FoodLogEntries] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_FoodLogEntries_MemberProfile_MemberProfileId] FOREIGN KEY ([MemberProfileId]) REFERENCES [dbo].[MemberProfile] ([Id])
 );
